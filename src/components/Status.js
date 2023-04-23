ae041139620b23bae7bacb2f63d65fbf0c65f630
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-04-23 18:17:13 +0545

    fixed static status problem

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,19 +1,23 @@
 function Status({ status }) {
   let classNames = "userStats__status ";
+  let label = "Inactive";
   switch (status) {
     case "active":
       classNames += "userStats__status--isActive";
+      label = "Active";
       break;
     case "blocked":
       classNames += "userStats__status--isBlocked";
+      label = "Blocked";
       break;
     case "in_active":
     default:
       classNames += "userStats__status--isNotActive";
+
       break;
   }
 
-  return <div className={classNames}>Active</div>;
+  return <div className={classNames}>{label}</div>;
 }
 
 export default Status;
